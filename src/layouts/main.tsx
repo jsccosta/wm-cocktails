d8ba5b9b95d24015b85677b7c49b705356bc6ce2
jsccosta <juliocosta1@gmail.com>
2022-06-17 14:11:21 +0100

    add dark mode button and icons for styling

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { Outlet, useOutlet } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { Navbar } from "../components";
 
 type MainLayoutProps = {
@@ -7,10 +10,28 @@ type MainLayoutProps = {
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
   const outlet = useOutlet();
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <div className="h-full bg-pampas" data-testid="main">
       <Navbar />
+
+      <button
+        data-testid="layout-darkModeButton"
+        aria-label={`Toggle dark mode ${darkMode ? "off" : "on"}`}
+        title="Toggle dark mode"
+        type="button"
+        onClick={() => {
+          setDarkMode(!darkMode);
+          console.log("dark");
+        }}
+      >
+        {darkMode ? (
+          <FontAwesomeIcon icon={faSun} color="#FFA500" />
+        ) : (
+          <FontAwesomeIcon icon={faMoon} />
+        )}
+      </button>
       <div className="px-8 py-6 sm:px-4">
         <div className="container mx-auto">
           {outlet ? <Outlet /> : children}
