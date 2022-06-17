ffcf44514702bfe251f8bc93233109b2a70abc67
jsccosta <juliocosta1@gmail.com>
2022-06-18 00:58:40 +0100

    add page not found

---
+++ src/pages/PageNotFound/index.tsx
@@ -0,0 +1,11 @@
+const PageNotFound = () => {
+  return (
+    <div className="space-y-3 rounded border border-gray-200 bg-white/25 p-5 text-sm dark:bg-gray-900 dark:text-white">
+      <p className="block">
+        <code className="text-sm font-light">Page not found!</code>.
+      </p>
+    </div>
+  );
+};
+
+export default PageNotFound;
