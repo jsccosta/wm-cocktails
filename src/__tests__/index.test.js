c65382d7a512b05730903dc14234c17f445054cf
jsccosta <juliocosta1@gmail.com>
2022-06-17 16:15:38 +0100

    check that application background color changes when clicking on button

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, userEvent, fireEvent } from "@testing-library/react";
 
 import App from "../app";
 
@@ -12,4 +12,16 @@ test("renders lazy component", async () => {
 test("displays dark mode button", () => {
   render(<App />);
   expect(screen.getByTestId("layout-darkModeButton")).toBeInTheDocument();
+});
+
+test("clicking on dark mode button toggles background color", async () => {
+  render(<App />);
+  const darkModeToggleButton = screen.getByTestId("layout-darkModeButton");
+  const applicationBackground = screen.getByTestId("main");
+
+  expect(applicationBackground).toHaveClass("bg-pampas");
+  expect(darkModeToggleButton).toBeInTheDocument();
+
+  await fireEvent.click(darkModeToggleButton);
+  expect(applicationBackground).toHaveClass("bg-gray-900");
 });
