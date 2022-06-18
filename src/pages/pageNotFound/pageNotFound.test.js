e7bfaa1dfb8d961670b0ff679b309a7eae73e308
jsccosta <juliocosta1@gmail.com>
2022-06-19 00:56:06 +0100

    add test for 404 page on bad route

--- src/pages/pageNotFound/pageNotFound.test.js
+++ src/pages/pageNotFound/pageNotFound.test.js
@@ -1,7 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import PageNotFound from "./index";
 import { ErrorMessage } from "./404Message";
+import { Router } from "../../router";
+
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
 
 it("shows page not found message", () => {
   render(
@@ -10,4 +20,16 @@ it("shows page not found message", () => {
     </BrowserRouter>
   );
   expect(screen.getByText(ErrorMessage)).toBeInTheDocument();
+});
+
+it("shows goes to 404 page on a bad path", async () => {
+  render(
+    <MemoryRouter initialEntries={["/some/bad/path"]}>
+      <Router />
+    </MemoryRouter>
+  );
+
+  await waitFor(() =>
+    expect(screen.getByText(ErrorMessage)).toBeInTheDocument()
+  );
 });
