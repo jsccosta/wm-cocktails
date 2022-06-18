09c7be49d9df71e765f714d2181051a9d1e1ef10
jsccosta <juliocosta1@gmail.com>
2022-06-19 00:02:53 +0100

    update 404 page error message, wrap component tested in browser router

--- src/pages/pageNotFound/pageNotFound.test.js
+++ src/pages/pageNotFound/pageNotFound.test.js
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import PageNotFound from "./index";
 
 it("shows page not found message", () => {
-  render(<PageNotFound />);
-  expect(screen.getByText("Page not found!")).toBeInTheDocument();
+  render(
+    <BrowserRouter>
+      <PageNotFound />
+    </BrowserRouter>
+  );
+  expect(
+    screen.getByText("Oh no! The page you were looking for was not found.")
+  ).toBeInTheDocument();
 });
