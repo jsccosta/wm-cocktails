dfba4f0e70d15761b06864c3a390e44f2b34953c
jsccosta <juliocosta1@gmail.com>
2022-06-20 18:49:06 +0100

    add router safeguard to redirect to 404 if user inputs mangled numbers into page query param

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams, Navigate } from "react-router-dom";
 import { MainLayout } from "./layouts/main";
 
 import { Homepage, Recipes, PageNotFound } from "./pages";
@@ -6,17 +6,36 @@ import { Homepage, Recipes, PageNotFound } from "./pages";
 export const ROUTES = {
   HOME: "/",
   RECIPES: "/recipes",
-  PAGEDRECIPES: "/recipes/:pageNumber",
+  PAGED_RECIPES: "/recipes/:pageNumber",
+  PAGE_NOT_FOUND: "/notFound",
+};
+
+const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
+  const { pageNumber } = useParams();
+  const parsedPageNumber = Number(pageNumber);
+  return typeof parsedPageNumber === "number" && !isNaN(parsedPageNumber) ? (
+    children
+  ) : (
+    <Navigate to="/notFound" />
+  );
 };
 
 export const Router = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="*" element={<PageNotFound />} />
         <Route path={ROUTES.HOME} element={<Homepage />} />
         <Route path={ROUTES.RECIPES} element={<Recipes />} />
-        <Route path={ROUTES.PAGEDRECIPES} element={<Recipes />} />
+        <Route
+          path={ROUTES.PAGED_RECIPES}
+          element={
+            <PrivateRoute>
+              <Recipes />
+            </PrivateRoute>
+          }
+        />
+        <Route path={ROUTES.PAGE_NOT_FOUND} element={<PageNotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
   );
